7be0f99d93054a2f8d1db943d0f2e94d07aa100d
willymateo <wjmateo@espol.edu.ec>
2024-12-01 15:53:34 -0500

    Move the readFileAndGetColumns to utils

--- day1/index.js
+++ day1/index.js
@@ -1,27 +1,4 @@
-const fs = require("node:fs/promises");
-
-const readFileAndGetColumns = async () => {
-  try {
-    const left = [];
-    const right = [];
-
-    const data = await fs.readFile("./data.txt", { encoding: "utf8" });
-    const rows = data.split("\n")?.filter(Boolean);
-
-    rows.forEach((rowStr) => {
-      const [columnA, columnB] = rowStr
-        ?.split(/\s+/)
-        ?.map((numberStr) => Number.parseInt(numberStr));
-
-      left.push(columnA);
-      right.push(columnB);
-    });
-
-    return { left, right };
-  } catch (err) {
-    console.log("Error reading the data file", err);
-  }
-};
+const { readFileAndGetColumns } = require("./utils");
 
 const calculateTotalDistance = ({ left = [], right = [] } = {}) => {
   const sortedLeft = left.sort();
