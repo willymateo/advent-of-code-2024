bb86c45709930cd38c7641be241e2853a3270b29
willymateo <wjmateo@espol.edu.ec>
2024-12-03 00:13:56 -0500

    day 2 - part 2

--- day2/part-1.py
+++ day2/part-1.py
@@ -5,25 +5,29 @@ data_path = os.path.join(os.getcwd(), "data.txt")
 reports_df = pd.read_csv(data_path, header=None, sep=" ", names=range(0, 8))
 
 
+def verify_levels_safety(levels):
+    safe = True
+
+    for index in range(levels.shape[0]):
+        if index == 0:
+            continue
+
+        diff = abs(levels.iloc[index - 1] - levels.iloc[index])
+
+        if diff == 0 or diff > 3:
+            safe = False
+            break
+
+    return safe
+
+
 def verify_report_safety(row):
     row_no_nan = row.dropna()
     row_asc = row_no_nan.sort_values(ascending=True)
     row_desc = row_no_nan.sort_values(ascending=False)
 
     if row_no_nan.equals(row_asc) or row_no_nan.equals(row_desc):
-        level_is_safe = True
-
-        for index in range(row_no_nan.shape[0]):
-            if index == 0:
-                continue
-
-            diff = abs(row_no_nan.iloc[index - 1] - row_no_nan.iloc[index])
-
-            if diff == 0 or diff > 3:
-                level_is_safe = False
-                break
-
-        return level_is_safe
+        return verify_levels_safety(row_no_nan)
 
     return False
 
