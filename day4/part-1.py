446faffe66fcade1efa9ccba40a2616dc738de3d
willymateo <wjmateo@espol.edu.ec>
2024-12-06 23:38:13 -0500

    Day 4 - part 1

---
+++ day4/part-1.py
@@ -0,0 +1,57 @@
+import numpy as np
+import os
+
+data_path = os.path.join(os.getcwd(), "data.txt")
+data_file = open(data_path, "r")
+word_search_str = data_file.read()
+
+
+def get_top_left_to_bottom_right_diagonals(matrix):
+    rows, cols = matrix.shape
+    diagonals = []
+
+    for offset in range(-rows + 1, cols):
+        diagonals.append(matrix.diagonal(offset))
+
+    return diagonals
+
+
+lines = word_search_str.split("\n")[:-1]
+lines_arr = np.array(lines)
+word_search_matrix = np.array([list(line) for line in lines])
+lines_inverted_arr = np.apply_along_axis("".join, axis=0, arr=word_search_matrix)
+diagonals = get_top_left_to_bottom_right_diagonals(word_search_matrix)
+diagonals_str_arr = np.array(["".join(diagonal) for diagonal in diagonals])
+diagonals_inverted = get_top_left_to_bottom_right_diagonals(word_search_matrix.T)
+diagonals_inverted_str_arr = np.array(
+    ["".join(diagonal) for diagonal in diagonals_inverted]
+)
+
+
+horizontal_founds = np.strings.count(lines_arr, "XMAS")
+horizontal_founds_total = np.sum(horizontal_founds)
+horizontal_inverted_founds = np.strings.count(lines_arr, "SAMX")
+horizontal_inverted_founds_total = np.sum(horizontal_inverted_founds)
+
+vertical_founds = np.strings.count(lines_arr, "XMAS")
+vertical_founds_total = np.sum(vertical_founds)
+vertical_inverted_founds = np.strings.count(lines_inverted_arr, "SAMX")
+vertical_inverted_founds_total = np.sum(vertical_inverted_founds)
+
+
+diagonals_found = np.strings.count(diagonals_str_arr, "XMAS")
+diagonals_found_total = np.sum(diagonals_found)
+diagonals_inverted_found = np.strings.count(diagonals_inverted_str_arr, "SAMX")
+diagonals_inverted_found_total = np.sum(diagonals_inverted_found)
+
+
+total_founds = (
+    horizontal_founds_total
+    + horizontal_inverted_founds_total
+    + vertical_founds_total
+    + vertical_inverted_founds_total
+    + diagonals_found_total
+    + diagonals_inverted_found_total
+)
+
+print(total_founds)
